0593f7c42202f2f78029094ee5860b352ebcc937
varunkashyap13 <vkash13@gmail.com>
2021-12-20 01:26:09 -0500

    Installed Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_b658787a96e74a29b40a1aa367a0d956')
+
+    if params[:ticker] == ""
+      @nothing = "Stock ticker symbol not entered."
+    elsif params[:ticker]
+
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue RuntimeError
+        @error = "Stock symbol doesn't exist. Please try again."
+      else 
+        p "No error"
+      ensure  
+        p "Done"
+      end
+    end
+  end
+
+  def about
   end
 end
